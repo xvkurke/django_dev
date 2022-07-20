308ca059d8c87d5ec53cc577b0e626f8d1c4f7d1
xvkurke <oleged.8896@gmail.com>
2022-07-20 20:01:53 +0300

    Hello world
    Hello world feature in site - completed-)

--- my_first_site/my_first_site/settings.py
+++ my_first_site/my_first_site/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dcr9ku&vz^-6+j^2vr0hjhudq6abm%w^85zihcb1)p6mm#no30'
+SECRET_KEY = 'django-insecure-tzy4uq6ubz5$^w1!h@swq@ool#+!^)_dy3ysaco4ux4#f$*=ch'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
