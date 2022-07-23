a4447d29424ad6093a186ef437c12e1b999d7a3b
xvkurke <oleged.8896@gmail.com>
2022-07-23 23:01:32 +0300

    Add index for html
     - Add start page for news
    
     - Update migrations

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import News
 
 # Create your views here.
 
 def index(request):
-    print(request)
-    return HttpResponse('Hello world!')
+    news = News.objects.all()
+    res = '<h1>List of news</h1>'
+    for item in news:
+        res += f'''
+        <div>
+            <p>{item.title}</p>\n
+            <p>{item.content}</p>\n
+        <div>
+        <hr>
+        '''
+    return HttpResponse(res)
 
 def test(request):
     return HttpResponse('<h1>Test</h1>')
