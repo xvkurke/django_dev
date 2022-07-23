962e59774fd6283aa096667557f0ac647474a1a8
xvkurke <oleged.8896@gmail.com>
2022-07-23 17:06:04 +0300

    add models for news app

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+class News(models.Model):
+    title = models.CharField( max_length = 150 )
+    content = models.TextField( blank = True )
+    created_at = models.DateTimeField( auto_now_add = True )
+    updated_at = models.DateTimeFiled( auto_now = True )
+    photo = models.ImageField( upload_to = 'photos/%Y/%m/%d/' )
+    is_published = models.BooleanField( default = True )
