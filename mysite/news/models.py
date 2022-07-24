5467d501ecafc53827bb895b73cb88641c7724ff
xvkurke <oleged.8896@gmail.com>
2022-07-24 14:31:47 +0300

    Add edit options for admin
     - Fixed model
     - Add meta for model
    
     - Add admin edit options
     - Add superuser

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -4,12 +4,17 @@ from django.db import models
 
 class News(models.Model):
     
-    title = models.CharField( max_length = 150 )
-    content = models.TextField( blank = True )
-    created_at = models.DateTimeField( auto_now_add = True )
-    updated_at = models.DateTimeField( auto_now = True )
-    photo = models.ImageField( upload_to = 'photos/%Y/%m/%d/' )
-    is_published = models.BooleanField( default = True )
+    title = models.CharField( max_length = 150 , verbose_name = 'Title' )
+    content = models.TextField( blank = True, verbose_name = 'Content' )
+    created_at = models.DateTimeField( auto_now_add = True, verbose_name = 'Created At' )
+    updated_at = models.DateTimeField( auto_now = True, verbose_name = 'Updated At' )
+    photo = models.ImageField( upload_to = 'photos/%Y/%m/%d/', blank = True, verbose_name = 'Photo')
+    is_published = models.BooleanField( default = True, verbose_name = 'Is Published' )
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'News list'
+        verbose_name_plural = 'News lists'
+        ordering = ['-created_at']
