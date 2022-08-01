5cbe5b096d9d4a9b0038f8aec3f18e59a5d5882f
xvkurke <oleged.8896@gmail.com>
2022-08-01 22:52:49 +0300

    Add constructor for templates
     - Optimized templates to build page with more little pages
    
     - Fixed trouble with incorrect work category pages

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),
+    path('', index, name = 'home'),
+    path('cat/<int:category_id>/', get_category, name = 'category'),
 ]
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import News
+from .models import News, Category
 
 # Create your views here.
 
 def index(request):
     news = News.objects.all()
-    return render(request, 'news/index.html', {'news': news, 'title': 'List of news'})
+    categories = Category.objects.all()
+    context = {
+        'news': news, 
+        'title': 'List of news',
+        'categories': categories,
+    }
+    return render(request, 'news/index.html', context = context)
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id = category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk = category_id)
+    context = {
+        'news': news,
+        'categories': categories,
+        'category': category,
+    }
+    return render(request, 'news/category.html', context = context)
