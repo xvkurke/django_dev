683968853243672ae634f448ee736da5233e830e
xvkurke <oleged.8896@gmail.com>
2022-08-02 20:55:55 +0300

    Add some custom tags
     - Little rework of code
     - Add custom tags to reduce amount of code

---
+++ mysite/news/templatetags/news_tags.py
@@ -0,0 +1,9 @@
+from django import template
+
+from news.models import Category
+
+register = template.Library()
+
+@register.simple_tag( name = 'get_list_categories' )
+def get_categories():
+    return Category.objects.all()
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -5,23 +5,23 @@ from .models import News, Category
 
 # Create your views here.
 
+
 def index(request):
     news = News.objects.all()
-    categories = Category.objects.all()
-    context = {
-        'news': news, 
-        'title': 'List of news',
-        'categories': categories,
-    }
-    return render(request, 'news/index.html', context = context)
-
-def get_category(request, category_id):
-    news = News.objects.filter(category_id = category_id)
-    categories = Category.objects.all()
-    category = Category.objects.get(pk = category_id)
     context = {
         'news': news,
-        'categories': categories,
+        'title': 'List of news',
+    }
+
+
+    return render(request, 'news/index.html', context=context)
+
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    category = Category.objects.get(pk=category_id)
+    context = {
+        'news': news,
         'category': category,
     }
-    return render(request, 'news/category.html', context = context)
+    return render(request, 'news/category.html', context=context)
